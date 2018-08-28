4b37bb442bc6f537df2df331f84aea8e5b54facf
sjmjys954646 <182605@jnu.ac.kr>
2018-08-28 20:37:57 +0900

    2018.08.28

---
+++ Today_Lunch/Assets/Scripts/CheckManager.cs
@@ -0,0 +1,215 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckManager : MonoBehaviour {
+
+    public GameObject First;
+    public GameObject Second;
+    public GameObject Third;
+    public GameObject Forth;
+    public GameObject DataBase;
+
+    public List<Database.Info> Selectedfirst = new List<Database.Info>();
+    public List<Database.Info> Selectedsecond = new List<Database.Info>();
+    public List<Database.Info> Selectedthird = new List<Database.Info>();
+    public List<Database.Info> Selectedforth = new List<Database.Info>();
+
+    public int count = 0;
+    public int result;
+    public Text resulttext;
+
+    public void StartRoulette()
+    {
+        if(First.transform.GetChild(1).GetComponent<Toggle>().isOn==true)
+        {
+            DataBase.GetComponent<Database>().Shop.ForEach(delegate (Database.Info go)
+            {
+                if(go.Foodkind == "한식")
+                {
+                    Selectedfirst.Add(go);
+                }
+            });
+        }
+
+        if (First.transform.GetChild(2).GetComponent<Toggle>().isOn == true)
+        {
+            DataBase.GetComponent<Database>().Shop.ForEach(delegate (Database.Info go)
+            {
+                if (go.Foodkind == "중식")
+                {
+                    Selectedfirst.Add(go);
+                }
+            });
+        }      
+
+        if (First.transform.GetChild(3).GetComponent<Toggle>().isOn == true)
+        {
+            DataBase.GetComponent<Database>().Shop.ForEach(delegate (Database.Info go)
+            {
+                if (go.Foodkind == "퓨전")
+                {
+                    Selectedfirst.Add(go);
+                }
+            });
+        }
+
+        if (First.transform.GetChild(4).GetComponent<Toggle>().isOn == true)
+        {
+            DataBase.GetComponent<Database>().Shop.ForEach(delegate (Database.Info go)
+            {
+                if (go.Foodkind == "치킨")
+                {
+                    Selectedfirst.Add(go);
+                }
+            });
+        }
+
+        if (First.transform.GetChild(5).GetComponent<Toggle>().isOn == true)
+        {
+            DataBase.GetComponent<Database>().Shop.ForEach(delegate (Database.Info go)
+            {                
+                if (go.Foodkind == "양식")
+                {
+                    Selectedfirst.Add(go);
+                }
+            });
+        }        
+
+        if (First.transform.GetChild(6).GetComponent<Toggle>().isOn == true)
+        {
+            DataBase.GetComponent<Database>().Shop.ForEach(delegate (Database.Info go)
+            {
+                if (go.Foodkind == "일식")
+                {
+                    Selectedfirst.Add(go);
+                }
+            });
+        }
+        
+        if (First.transform.GetChild(7).GetComponent<Toggle>().isOn == true)
+        {
+            DataBase.GetComponent<Database>().Shop.ForEach(delegate (Database.Info go)
+            {             
+                if (go.Foodkind == "분식")
+                {
+                    Selectedfirst.Add(go);
+                }
+            });
+        }
+
+        if (First.transform.GetChild(8).GetComponent<Toggle>().isOn == true)
+        {
+            DataBase.GetComponent<Database>().Shop.ForEach(delegate (Database.Info go)
+            {
+                if (go.Foodkind == "고깃집")
+                {
+                    Selectedfirst.Add(go);
+                }
+            });
+        }
+
+        if (First.transform.GetChild(9).GetComponent<Toggle>().isOn == true)
+        {
+            DataBase.GetComponent<Database>().Shop.ForEach(delegate (Database.Info go)
+            { 
+                if (go.Foodkind == "후식")
+                {
+                    Selectedfirst.Add(go);
+                }
+            });
+        }
+
+        if (Second.transform.GetChild(1).GetComponent<Toggle>().isOn == true)
+        {
+            Selectedfirst.ForEach(delegate (Database.Info go)
+            {
+                if (go.Pos == "후문")
+                {
+                    Selectedsecond.Add(go);
+                }
+            });
+        }
+        
+        if (Second.transform.GetChild(2).GetComponent<Toggle>().isOn == true)
+        {
+            Selectedfirst.ForEach(delegate (Database.Info go)
+            {
+                if (go.Pos == "쪽문")
+                {
+                    Selectedsecond.Add(go);
+                }
+            });
+        }
+
+        if (Third.transform.GetChild(1).GetComponent<Toggle>().isOn == true)
+        {
+            Selectedsecond.ForEach(delegate (Database.Info go)
+            {
+                if (go.Price == "하")
+                {
+                    Selectedthird.Add(go);
+                }
+            });
+        }
+
+        if (Third.transform.GetChild(2).GetComponent<Toggle>().isOn == true)
+        {
+            Selectedsecond.ForEach(delegate (Database.Info go)
+            {
+                if (go.Price == "중")
+                {
+                    Selectedthird.Add(go);
+                }
+            });
+        }
+
+        if (Third.transform.GetChild(3).GetComponent<Toggle>().isOn == true)
+        {
+            Selectedsecond.ForEach(delegate (Database.Info go)
+            {
+                if (go.Price == "상")
+                {
+                    Selectedthird.Add(go);
+                }
+            });
+        }       
+
+        if (Forth.transform.GetChild(1).GetComponent<Toggle>().isOn == true)
+        {
+            Selectedthird.ForEach(delegate (Database.Info go)
+            {  
+                Selectedforth.Add(go);                
+            });
+        }
+        else
+        {
+            Selectedthird.ForEach(delegate (Database.Info go)
+            {
+                if (go.Deliever == false)
+                {
+                    Selectedforth.Add(go);
+                }
+            });
+        }
+
+        Selectedforth.ForEach(delegate (Database.Info go)
+        {
+            Debug.Log(go.Storename);
+        });
+    }   
+
+    public void Pickrandom()
+    {
+        Selectedforth.ForEach(delegate (Database.Info go)
+        {
+            count += 1;
+        });
+        Debug.Log(count);
+        result = Random.Range(0, count);
+        Debug.Log(result);
+        resulttext.text = Selectedforth[result].Storename;
+
+    }
+}
---
+++ Today_Lunch/Assets/Scripts/Database.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Database : MonoBehaviour
+{
+    public List<Info> Shop = new List<Info>();
+
+    //1.몇번 2.식당이름 3.음식종류 4.위치 5.가격 6.배달음식
+    public class Info
+    {
+        public int Num;
+        public string Storename, Foodkind, Pos, Price;
+        public bool Deliever;
+
+        public Info(int a, string b, string c, string d, string e, bool f)
+        {
+            Num = a;
+            Storename = b;
+            Foodkind = c;
+            Pos = d;
+            Price = e;
+            Deliever = f;
+        }
+    }
+
+    private void Start()
+    {
+        WriteInfo();
+    }
+   
+    public void WriteInfo()
+    {
+        Shop.Add(new Info(1,"스피드반점", "중식", "쪽문", "하", false));
+        Shop.Add(new Info(2, "밥스", "퓨전", "쪽문", "하", false));
+        Shop.Add(new Info(3, "털보하우스", "퓨전", "쪽문", "하", false));
+        Shop.Add(new Info(4, "용봉골", "한식", "쪽문", "하", false));
+        Shop.Add(new Info(5, "얌얌", "양식", "쪽문", "중", false));
+        Shop.Add(new Info(6, "석쇠쌈밥", "한식", "쪽문", "하", false));
+        Shop.Add(new Info(7, "부리또", "양식", "쪽문", "하", false));
+        Shop.Add(new Info(8, "사랑방하우스", "한식", "쪽문", "하", false));
+        Shop.Add(new Info(9, "나들목국밥", "한식", "쪽문", "중", false));
+        Shop.Add(new Info(10, "파리에다녀온치킨", "치킨", "쪽문", "상", false));
+    }
+}
+
+
